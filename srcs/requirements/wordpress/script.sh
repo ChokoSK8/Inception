c315059fdb652c78131a153e3f219ccd5281fb6e
ChokoSK8 <antoine.brunSK8@live.fr>
2022-06-01 17:36:31 +0200

    email pb to resolve

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -9,7 +9,7 @@ mkdir -p /run/php/;
 touch /run/php/php7.3-fpm.pid;
 
 cd /var/www/site
-wp core install --url=${DOMAIN_NAME} --title="HEY YOU" --admin_user=${WP_ADMIN_USERNAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
+wp core install --url=${DOMAIN_NAME} --title=wpData --admin_user=${WP_ADMIN_USERNAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
 wp user create ${WP_USER} --user_pass=${WP_USER_PASSWORD} --allow-root
 
 exec "$@"
