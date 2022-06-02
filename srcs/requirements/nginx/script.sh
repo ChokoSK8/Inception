6ba34a2ea713e187b4b4455f882c8b9139e0ee49
ChokoSK8 <antoine.brunSK8@live.fr>
2022-06-02 14:04:13 +0200

    almost done

--- srcs/requirements/nginx/script.sh
+++ srcs/requirements/nginx/script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+if [ ! -f /etc/ssl/private/abrun.key ];
+then
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/abrun.key -out /etc/ssl/certs/abrun.crt -subj "/C=FR/ST=Paris/L=IDF/O=42School/CN=abrun.42.fr";
+fi
 exec "$@"
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-#sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+if [ ! -f /run/php/php7.3-fpm.pid ];
+then
+	cat /run/php/php7.3-fpm.pid
+	chown -R www-data:www-data /var/www/*
+	chown -R 755 /var/www/*
 
-chown -R www-data:www-data /var/www/*
-chown -R 755 /var/www/*
+	cd /var/www/site
+	wp core install --url=${DOMAIN_NAME} --title=wpData --admin_user=${WP_ADMIN_USERNAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
+	wp user create ${WP_USER} ${WP_USER_EMAIL} --role="editor" --user_pass=${WP_USER_PASSWORD} --allow-root
 
-mkdir -p /run/php/;
-touch /run/php/php7.3-fpm.pid;
-
-cd /var/www/site
-wp core install --url=${DOMAIN_NAME} --title=wpData --admin_user=${WP_ADMIN_USERNAME} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
-wp user create ${WP_USER} --user_pass=${WP_USER_PASSWORD} --allow-root
+	mkdir -p /run/php/;
+	touch /run/php/php7.3-fpm.pid;
+fi
 
 exec "$@"
